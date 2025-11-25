7a15adae1f82256e5d0be9218e594e7c4cffd693
JERICHO URBANO <119677296+shiehb@users.noreply.github.com>
2025-11-26 03:36:28 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /app/server
+# No cd command needed since WORKDIR is already set to /app/server
 exec gunicorn core.wsgi:application --bind 0.0.0.0:$PORT --workers 2 --threads 2 --timeout 120
